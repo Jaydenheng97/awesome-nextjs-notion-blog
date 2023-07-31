e8f0c6519b97af030bfc9e67c7efe367f6687c91
Jaydenheng97 <87332717+Jaydenheng97@users.noreply.github.com>
2023-07-31 23:51:03 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,10 +18,10 @@ module.exports = {
 
   // social usernames (optional)
   twitter: null,
-  github: 'frankcbliu',
-  wechatPublicName: '菜饼不菜',
-  wechatPublicURL: 'https://mp.weixin.qq.com/s?__biz=MzkxMzIyOTI1Mg==&mid=2247486027&idx=1&sn=2840f0b8cfdc982b9095dd18ec9892e5',
-  notionPublic: 'https://frankcbliu.notion.site/6a76fdd9ec6d4ae2855aa790d620eec4',
+  github: null,
+  wechatPublicName: null,
+  wechatPublicURL: null,
+  notionPublic: null,
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
