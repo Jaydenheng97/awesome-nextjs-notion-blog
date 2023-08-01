bd6fbda1fb46d5e6d42d8aa2fa7a436af7d30dd1
Jaydenheng97 <87332717+Jaydenheng97@users.noreply.github.com>
2023-08-01 20:45:45 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -43,7 +43,7 @@ module.exports = {
   isPreviewImageSupportEnabled: false,
 
   // set null to remove from year
-  copyRightFromYear: 2019,
+  copyRightFromYear: null,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
